cddbc005724210dfdb5938d36240df81294a9a05
Cheng Ye <2540253y@student.gla.ac.uk>
2021-07-28 18:14:06 +0100

    six commit

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
-    
+
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Cheng Ye'}
+    return render(request, 'rango/about.html', context=context_dict)
